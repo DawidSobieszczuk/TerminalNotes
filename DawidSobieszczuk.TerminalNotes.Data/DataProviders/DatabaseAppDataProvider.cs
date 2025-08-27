c72598db54e3ec947f8c387a87fc59292b35f882
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 12:41:46 +0200

    feat: create AppDataProvider

---
+++ DawidSobieszczuk.TerminalNotes.Data/DataProviders/DatabaseAppDataProvider.cs
@@ -0,0 +1,36 @@
+﻿using DawidSobieszczuk.TerminalNotes.Data.Interfaces;
+using DawidSobieszczuk.TerminalNotes.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DawidSobieszczuk.TerminalNotes.Data.DataProviders
+{
+    public class DatabaseAppDataProvider(IDbContextFactory<AppDbContext> dbContextFactory) : IAppDataProvider
+    {
+        public async Task DeleteNoteAsync(Note note)
+        {
+            AppDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            dbContext.Notes.Remove(note);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IQueryable<Note>> GetNotesAsync()
+        {
+            AppDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            return dbContext.Notes.AsNoTracking();
+        }
+
+        public async Task InsertNoteAsync(Note note)
+        {
+            AppDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            await dbContext.Notes.AddAsync(note);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateNoteAsync(Note note)
+        {
+            AppDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            dbContext.Notes.Update(note);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ DawidSobieszczuk.TerminalNotes.Data/Interfaces/IAppDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using DawidSobieszczuk.TerminalNotes.Data.Models;
+
+namespace DawidSobieszczuk.TerminalNotes.Data.Interfaces
+{
+    public interface IAppDataProvider
+    {
+        public Task<IQueryable<Note>> GetNotesAsync();
+        public Task UpdateNoteAsync(Note note);
+        public Task InsertNoteAsync(Note note);
+        public Task DeleteNoteAsync(Note note);
+    }
+}
