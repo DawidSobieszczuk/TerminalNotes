aa763e9b2513101c90294bab5890a39319568294
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 10:06:57 +0200

    feat: create Note model

---
+++ DawidSobieszczuk.TerminalNotes.Data/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace DawidSobieszczuk.TerminalNotes.Data.Models
+{
+    public class Note
+    {
+        public required int Id { get; set; }
+        public required string Content { get; set; }
+        public required string[] Tags { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime UpdatedAt { get; set; }
+    }
+}
