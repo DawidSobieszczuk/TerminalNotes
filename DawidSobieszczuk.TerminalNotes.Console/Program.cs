7e46c4242a0ba4192308056d6935752d59e44e2d
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 12:07:07 +0200

    feat: add logic to create the notes database file

--- DawidSobieszczuk.TerminalNotes.Console/Program.cs
+++ DawidSobieszczuk.TerminalNotes.Console/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using DawidSobieszczuk.TerminalNotes.Console.Services;
+using DawidSobieszczuk.TerminalNotes.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DawidSobieszczuk.TerminalNotes.Console
 {
@@ -9,6 +11,15 @@ namespace DawidSobieszczuk.TerminalNotes.Console
     {       
         public static int Main(string[] args)
         {
+            var dbFilePath = Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                            ".tnotes");
+
+            if (!Directory.Exists(dbFilePath))
+            {
+                Directory.CreateDirectory(dbFilePath);
+            }
+
             var host = Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
@@ -18,8 +29,18 @@ namespace DawidSobieszczuk.TerminalNotes.Console
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddTransient<ConsoleService>();
+                    services.AddDbContext<AppDbContext>(options => { 
+                        options.UseSqlite("Data Source =" + Path.Combine(dbFilePath,
+                            hostContext.Configuration["DatabaseSettings:DbFileName"] ?? "data.db"
+                            ));
+                    });
                 })
                 .Build();
+
+            using var serviceScope = host.Services.CreateScope();
+
+            var dbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.Migrate();
 
             var consoleService = host.Services.GetRequiredService<ConsoleService>();
             return consoleService.Run(args);
