78c28fbfe2f9e3839e2d577d8ead2d6cb040c2c5
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 10:27:44 +0200

    feat: create AppDbContext

---
+++ DawidSobieszczuk.TerminalNotes.Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DawidSobieszczuk.TerminalNotes.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Models.Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Note>()
+                .Property(n => n.Tags)
+                .HasConversion(
+                    v => string.Join(',', v),
+                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
