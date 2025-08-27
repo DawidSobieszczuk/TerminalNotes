e85b1ed3d8e982913fb3f4126b798d32788b66c1
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 12:52:21 +0200

    fix: remote required from UpdatedAt property

--- DawidSobieszczuk.TerminalNotes.Data/Models/Note.cs
+++ DawidSobieszczuk.TerminalNotes.Data/Models/Note.cs
@@ -6,6 +6,6 @@
         public required string Content { get; set; }
         public required List<string> Tags { get; set; }
         public required DateTime CreatedAt { get; set; }
-        public required DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
