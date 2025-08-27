9f695e957909020f61b69a72e9bce6fb098d2e3c
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 12:05:10 +0200

    refactor: Change Tags property from array to list

--- DawidSobieszczuk.TerminalNotes.Data/Models/Note.cs
+++ DawidSobieszczuk.TerminalNotes.Data/Models/Note.cs
@@ -4,7 +4,7 @@
     {
         public required int Id { get; set; }
         public required string Content { get; set; }
-        public required string[] Tags { get; set; }
+        public required List<string> Tags { get; set; }
         public required DateTime CreatedAt { get; set; }
         public required DateTime UpdatedAt { get; set; }
     }
