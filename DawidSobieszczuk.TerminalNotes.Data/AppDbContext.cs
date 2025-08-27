07cc16c8a34a91ac8e48b4c2290fde910f651411
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-27 12:08:14 +0200

    refactor: update DbContext for DI and fix tags conversion

--- DawidSobieszczuk.TerminalNotes.Data/AppDbContext.cs
+++ DawidSobieszczuk.TerminalNotes.Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace DawidSobieszczuk.TerminalNotes.Data
 {
@@ -6,13 +7,22 @@ namespace DawidSobieszczuk.TerminalNotes.Data
     {
         public DbSet<Models.Note> Notes { get; set; }
 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Models.Note>()
                 .Property(n => n.Tags)
                 .HasConversion(
                     v => string.Join(',', v),
-                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries));
+                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToList())
+                .Metadata.SetValueComparer(new ValueComparer<List<string>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => c.ToList()
+                ));
         }
     }
 }
