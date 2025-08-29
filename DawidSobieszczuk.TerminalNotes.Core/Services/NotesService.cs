9c2a37976cffa529be6ce436dffdac4e44716532
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-08-29 13:22:08 +0200

    feat: create NotesService

---
+++ DawidSobieszczuk.TerminalNotes.Core/Services/NotesService.cs
@@ -0,0 +1,21 @@
+﻿using DawidSobieszczuk.TerminalNotes.Data.Interfaces;
+using DawidSobieszczuk.TerminalNotes.Data.Models;
+using System.Diagnostics;
+
+namespace DawidSobieszczuk.TerminalNotes.Core.Services
+{
+    public class NotesService(IAppDataProvider appDataProvider)
+    {
+        public async Task AddNote(string content, List<string>? tags = null)
+        {
+            var note = new Note
+            {
+                Id = 0,
+                Content = content,
+                Tags = tags,
+                CreatedAt = DateTime.UtcNow,
+            };
+            await appDataProvider.InsertNoteAsync(note);
+        }
+    }
+}
