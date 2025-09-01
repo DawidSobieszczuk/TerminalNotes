f42e0fe04c26cc6d92f7f0f2644f0be542199239
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-09-01 08:52:41 +0200

    feat: add default command

---
+++ DawidSobieszczuk.TerminalNotes.Console/Commands/RunCommand.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console.Cli;
+
+namespace DawidSobieszczuk.TerminalNotes.Console.Commands
+{
+    internal class RunCommand : Command<RunCommand.Settings>
+    {
+        public class Settings : CommandSettings
+        {
+            // Define any settings or options for the command here
+        }
+        public override int Execute(CommandContext context, Settings settings)
+        {
+            return 0;
+        }
+    }
+}
